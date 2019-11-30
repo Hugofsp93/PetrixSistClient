c5b351f12f2b1f4661bd19587603b78c8d2cdb1d
Victor <mr_pio@hotmail.com>
2019-11-30 15:00:37 +0000

    Web

---
+++ PetrixSisClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetrixSisClient.MvcApplication" Language="C#" %>
---
+++ PetrixSisClient/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetrixSisClient.Models
+{
+    public class Animal
+    {
+        public Animal()
+        {
+           // Anuncio = new HashSet<Anuncio>();
+        }
+
+        [Key]
+        public int id_animal { get; set; }
+
+        [StringLength(255)]
+        [Required(ErrorMessage ="informe o nome do animal")]
+        public string ds_animal { get; set; }
+
+       // public virtual ICollection<Anuncio> Anuncio { get; set; }
+    }
+}
