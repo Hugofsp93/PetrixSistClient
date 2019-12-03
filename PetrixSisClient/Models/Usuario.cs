a176ec2cad7d435e6f1f9d4d4a7310aa2a4e4c4c
Victor <mr_pio@hotmail.com>
2019-12-03 17:52:05 +0000

    Tela de usuarios e edicao do scaffold templates default

---
+++ PetrixSisClient/Models/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetrixSisClient.Models
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+            Anuncio = new HashSet<Anuncio>();
+        }
+
+        [Key]
+        public int id_user { get; set; }
+
+        [StringLength(255)]
+        public string email_user { get; set; }
+
+        [StringLength(255)]
+        public string endereco_user { get; set; }
+
+        [StringLength(255)]
+        public string nome_user { get; set; }
+
+        [StringLength(255)]
+        public string cfp_user { get; set; }
+
+        [StringLength(255)]
+        public string cnpj_user { get; set; }
+
+        [StringLength(255)]
+        public string site_user { get; set; }
+
+        public int? fk_Tipo_id_tipo { get; set; }
+
+        public virtual ICollection<Anuncio> Anuncio { get; set; }
+
+        public virtual Tipo Tipo { get; set; }
+    }
+}
